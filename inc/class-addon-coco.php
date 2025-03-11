4bebb7f7379095448bc708036e8398a46483faac
Cobianzo <cobianzo@gmail.com>
2025-03-10 23:45:07 -0600

    Refactor to Advanced Marker and added Map ID setting field

--- inc/class-addon-coco.php
+++ inc/class-addon-coco.php
@@ -14,7 +14,7 @@ if ( ! class_exists( 'GFAddOn' ) ) {
 
 class Addon_Coco extends \GFAddOn {
 
-	protected $_version                  = '4.0.1';
+	protected $_version                  = '5.0.0';
 	protected $_min_gravityforms_version = '2.5';
 
 	/**
